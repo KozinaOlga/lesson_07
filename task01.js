6a9294996fb952cd6e4f2ed2bb6ab104bad50e52
KozinaOlga <kozinaov.27@gmail.com>
2024-02-26 22:27:25 +1000

    задачи с массивами

---
+++ task01.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//__________-первый вариант
+
+const length = 100;
+
+const numbres = [...Array(length)];
+
+const getRendomNunbers = (numbres) => {
+    for (let i = 0; i < numbres.length; i++) {
+        numbres[i] = Math.trunc(Math.random() * 100);
+        }
+        return numbres
+}
+const result = getRendomNunbers(numbres)
+console.log('result: ', result);
+
+//__________-второй вариант
+
+const generateArray = (length) => (
+    [...new Array(length)]
+    .map(() => Math.trunc(Math.random() * 100))
+);
+
+const num = generateArray(1, 100);
+console.log(num);
